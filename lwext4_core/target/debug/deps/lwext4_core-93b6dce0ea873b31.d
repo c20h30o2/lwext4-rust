c6c989e1ab947f0205c335f08ca1907af2aa6bd6
c2h30o2 <2143957645@qq.com>
2025-12-08 21:34:20 +0800

    feat(cache): implement block cache with LRU eviction
    Implementation:
    - Add CacheBuffer with flags and reference counting
    - Add BlockCache with dual BTreeMap indexing (LBA + LRU)
    - Implement dirty block tracking and flush mechanism
    - Support range invalidation and async write callbacks
    
    Testing:
    - All 22 cache tests passing
    - 100% feature parity with lwext4 bcache

--- lwext4_core/target/debug/deps/lwext4_core-93b6dce0ea873b31.d
+++ lwext4_core/target/debug/deps/lwext4_core-93b6dce0ea873b31.d
@@ -1,14 +1,27 @@
-/home/c20h30o2/files/lwext4-rust/lwext4-rust/lwext4_core/target/debug/deps/lwext4_core-93b6dce0ea873b31.d: src/lib.rs src/consts.rs src/types.rs src/error.rs src/traits.rs src/superblock.rs src/inode.rs src/block.rs src/dir.rs src/fs.rs
+/home/c20h30o2/files/lwext4-rust/lwext4-rust/lwext4_core/target/debug/deps/lwext4_core-93b6dce0ea873b31.d: src/lib.rs src/error.rs src/block/mod.rs src/block/device.rs src/block/io.rs src/consts.rs src/types.rs src/superblock/mod.rs src/superblock/read.rs src/inode/mod.rs src/inode/read.rs src/extent/mod.rs src/extent/tree.rs src/dir/mod.rs src/dir/entry.rs src/dir/lookup.rs src/fs/mod.rs src/fs/filesystem.rs src/fs/file.rs src/fs/metadata.rs src/cache/mod.rs src/cache/buffer.rs src/cache/block_cache.rs
 
-/home/c20h30o2/files/lwext4-rust/lwext4-rust/lwext4_core/target/debug/deps/liblwext4_core-93b6dce0ea873b31.rmeta: src/lib.rs src/consts.rs src/types.rs src/error.rs src/traits.rs src/superblock.rs src/inode.rs src/block.rs src/dir.rs src/fs.rs
+/home/c20h30o2/files/lwext4-rust/lwext4-rust/lwext4_core/target/debug/deps/liblwext4_core-93b6dce0ea873b31.rmeta: src/lib.rs src/error.rs src/block/mod.rs src/block/device.rs src/block/io.rs src/consts.rs src/types.rs src/superblock/mod.rs src/superblock/read.rs src/inode/mod.rs src/inode/read.rs src/extent/mod.rs src/extent/tree.rs src/dir/mod.rs src/dir/entry.rs src/dir/lookup.rs src/fs/mod.rs src/fs/filesystem.rs src/fs/file.rs src/fs/metadata.rs src/cache/mod.rs src/cache/buffer.rs src/cache/block_cache.rs
 
 src/lib.rs:
+src/error.rs:
+src/block/mod.rs:
+src/block/device.rs:
+src/block/io.rs:
 src/consts.rs:
 src/types.rs:
-src/error.rs:
-src/traits.rs:
-src/superblock.rs:
-src/inode.rs:
-src/block.rs:
-src/dir.rs:
-src/fs.rs:
+src/superblock/mod.rs:
+src/superblock/read.rs:
+src/inode/mod.rs:
+src/inode/read.rs:
+src/extent/mod.rs:
+src/extent/tree.rs:
+src/dir/mod.rs:
+src/dir/entry.rs:
+src/dir/lookup.rs:
+src/fs/mod.rs:
+src/fs/filesystem.rs:
+src/fs/file.rs:
+src/fs/metadata.rs:
+src/cache/mod.rs:
+src/cache/buffer.rs:
+src/cache/block_cache.rs:
